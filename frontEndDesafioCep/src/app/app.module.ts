20ca374e19f806426ec42016958135d4df3153b1
João Henrique <joaohnsousa@gmail.com>
2023-11-22 12:04:23 -0300

    Criando Tela de Login

--- frontEndDesafioCep/src/app/app.module.ts
+++ frontEndDesafioCep/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontEndDesafioCep/src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  onSubmit() {
+    // Adicione lógica de autenticação aqui
+    console.log('Login attempt with:', this.username, this.password);
+  }
+}
